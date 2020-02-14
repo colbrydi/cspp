725ef5048c00de8b7dc16717e99e7e9e0b9aa4d9
shaylafolson <47156578+shaylafolson@users.noreply.github.com>
2020-02-14 17:19:46 -0500

    Initial commit

---
+++ R/cspp-full.R
@@ -0,0 +1,27 @@
+#' Full data from IPPSR Correlates of State Policy Project v2.2
+#' 
+#' The Correlates of State Policy Project includes more than 2000 variables, 
+#'  with observations across the 50 U.S. states and across time (1900-2016, approximately).
+#'  These variables represent policy outputs or political, social, or economic factors 
+#'  that may influence policy differences. The codebook includes the variable name, 
+#'  a short description of the variable, the variable time frame, 
+#'  a longer description of the variable, and the variable source(s) and notes.
+#'  
+#' This aggregated dataset is only possible because many scholars and students have spent
+#'  countless hours creating, collecting, cleaning, and making data publicly available. 
+#'  Thus, if you use the dataset, please cite the original data sources.
+#'  
+#'  @docType data
+#'  
+#'  @usage data(cspp-full)
+#'  
+#'  @format A data frame of 6120 rows and 2091 variables. Each observation is year-state.
+#'  
+#'  @keywords datasets
+#'  
+#'  @references 
+#'  Jordan, Marty P. and Matt Grossmann. 2020. The Correlates of State Policy Project v.2.2. 
+#'   East Lansing, MI: Institute for Public Policy and Social Research (IPPSR).
+#'   
+#'  @source \href{http://ippsr.msu.edu/public-policy/correlates-state-policy}
+"cspp-full"
